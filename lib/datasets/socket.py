d6275b3ca40f37d1343c5e9302d2705cfe32d5ed
VincentZhang <vincent.zhang@ualberta.ca>
2019-06-08 17:15:12 -0600

    update docstring for dataloader and clean up unused packages

--- lib/datasets/socket.py
+++ lib/datasets/socket.py
@@ -4,23 +4,22 @@
 # Licensed under The MIT License [see LICENSE for details]
 # Written by Ross Girshick
 # --------------------------------------------------------
+""" Dataloader for accetabulum(socket) data
+
+    Written by Vincent Zhang.
+"""
 
 import os
 from datasets.imdb import imdb
-import datasets.ds_utils as ds_utils
-import xml.etree.ElementTree as ET
 import numpy as np
 import scipy.sparse
 import scipy.io as sio
-import utils.cython_bbox
 import cPickle
 import subprocess
 import uuid
 from voc_eval import voc_eval
 from fast_rcnn.config import cfg
-import PIL
 import h5py
-import pdb
 
 
 class socket(imdb):
